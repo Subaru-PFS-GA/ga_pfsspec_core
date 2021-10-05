fd25c6cb354dd39a939cb17f8b2625ddd7253299
Laszlo Dobos <dobos@complex.elte.hu>
2021-10-05 04:55:07 -0400

    Reorganizing namespaces.

---
+++ pfsspec/core/io/exporter.py
@@ -0,0 +1,14 @@
+from pfsspec.common.pfsobject import PfsObject
+
+class Exporter(PfsObject):
+    def __init__(self, orig=None):
+        super(Importer, self).__init__()
+
+    def add_args(self, parser):
+        pass
+
+    def init_from_args(self, args):
+        pass
+
+    def execute_notebooks(self, script):
+        pass

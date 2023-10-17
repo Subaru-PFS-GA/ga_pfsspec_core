2f1bb9a3b8dbab8d4ff26aa5dddc3b6ae34ab0bd
Laszlo Dobos <dobos@jhu.edu>
2023-10-17 12:26:39 -0400

    Use masks in Stoehr el al. S/N calculation.

--- python/pfs/ga/pfsspec/core/obsmod/snr/stoehrsnr.py
+++ python/pfs/ga/pfsspec/core/obsmod/snr/stoehrsnr.py
@@ -3,13 +3,13 @@ import numpy as np
 from .snr import Snr
 
 class StoehrSnr(Snr):
-    def __init__(self, shift=2, binning=1.0, orig=None):
+    def __init__(self, binning=1.0, squared=None, shift=2, orig=None):
         """
         Estimate the S/N using Stoehr et al ADASS 2008
            
         signal = median(flux(i))
         noise = 1.482602 / sqrt(6.0) *
-        median(abs(2 * flux(i) - flux(i-2) - flux(i+2)))
+                median(abs(2 * flux(i) - flux(i-2) - flux(i+2)))
         DER_SNR = signal / noise
         """
 
@@ -17,19 +17,18 @@ class StoehrSnr(Snr):
         #       of the maximum filter involved in the the sky subtraction
         #       error estimate.
         
-        super().__init__(binning, orig=orig)
+        super().__init__(binning=binning, squared=squared, orig=orig)
 
         if not isinstance(orig, StoehrSnr):
             self.shift = shift if shift is None else 2
         else:
-            self.shift = shift if shift is not None else orig.q
+            self.shift = shift if shift is not None else orig.shift
 
-    def get_snr(self, value, sigma=None):
-        raise NotImplementedError()
-        # TODO: update to handle lists of values/sigmas and mask
+    def get_snr(self, value, sigma, mask):
+        # TODO: Handle cases when the mask would cause taking the difference of non-adjacent bins
 
-        s1 = np.median(value)
-        s2 = np.abs(2 * value[self.shift:-self.shift] - value[:-2 * self.shift] - value[2 * self.shift:])
-        n1 = 1.482602 / np.sqrt(6.0) * np.median(s2)
-        snr = s1 / n1 * np.sqrt(self.binning)
+        signal = np.median(value[mask])
+        df = np.abs(2 * value[mask][self.shift:-self.shift] - value[mask][:-2 * self.shift] - value[mask][2 * self.shift:])
+        noise = 1.482602 / np.sqrt(6.0) * np.median(df)
+        snr = signal / noise * np.sqrt(self.binning)
         return snr

49ad202825dfdd1cfe5fab9c86a943047a3b8a97
Laszlo Dobos <dobos@jhu.edu>
2024-05-14 16:41:15 -0400

    Pivot dictionaries.

--- python/pfs/ga/pfsspec/core/util/dict.py
+++ python/pfs/ga/pfsspec/core/util/dict.py
@@ -24,3 +24,32 @@ def safe_deep_compare(a, b, ignore=None):
             return False
 
     return True
+
+def pivot_dict_of_arrays(d):
+    """
+    Pivot a dictionary of arrays into an array of dictionaries
+    """
+
+    keys = list(d.keys())
+    n = len(d[keys[0]])
+    res = [{} for i in range(n)]
+
+    for k in keys:
+        for i in range(n):
+            res[i][k] = d[k][i]
+
+    return res
+
+def pivot_array_of_dicts(a):
+    """
+    Pivot an array of dictionaries into a dictionary of arrays
+    """
+
+    keys = a[0].keys()
+    res = { k: [] for k in keys }
+
+    for d in a:
+        for k in keys:
+            res[k].append(d[k])
+
+    return res

5f903096a88fd679dc6392f114f5a6df1f7812a4
Laszlo Dobos <dobos@jhu.edu>
2025-04-22 04:06:31 -0400

    Added snr by name map.

--- python/pfs/ga/pfsspec/core/obsmod/snr/__init__.py
+++ python/pfs/ga/pfsspec/core/obsmod/snr/__init__.py
@@ -2,3 +2,10 @@ from .meansnr import MeanSnr
 from .mediansnr import MedianSnr
 from .quantilesnr import QuantileSnr
 from .stoehrsnr import StoehrSnr
+
+SNR_TYPES = {
+    'mean': MeanSnr,
+    'median': MedianSnr,
+    'quantile': QuantileSnr,
+    'stoehr': StoehrSnr
+}
